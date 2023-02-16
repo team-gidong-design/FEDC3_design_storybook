ed805d661da9f4494c3210f408ca42d1dbee8703
Yuri Kim <glassk0724@gmail.com>
2023-02-16 12:27:03 +0900

    feature: Input 컴포넌트, 스토리 (#5)
    * feature: Input 컴포넌트, 스토리
    
    * refactor: props type 이름 변경, 명시적인 prop만 사용

---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,35 @@
+import Input from '@/src/components/Input';
+import { InputProps } from '@/types/input';
+
+export default {
+  title: 'Component/Input',
+  component: Input,
+  argTypes: {
+    label: {
+      defaultValue: 'Label',
+      control: 'text',
+    },
+    block: {
+      defaultValue: false,
+      control: 'boolean',
+    },
+    invalid: {
+      defaultValue: false,
+      control: 'boolean',
+    },
+    required: {
+      defaultValue: false,
+      control: 'boolean',
+    },
+    disabled: {
+      defaultValue: false,
+      control: 'boolean',
+    },
+    readOnly: {
+      defaultValue: false,
+      control: 'boolean',
+    },
+  },
+};
+
+export const Default = (args: InputProps) => <Input {...args} />;
---
+++ src/types/input.d.ts
@@ -0,0 +1,8 @@
+export type InputProps = {
+  label: string;
+  block: boolean;
+  invalid: boolean;
+  required: boolean;
+  disabled: boolean;
+  readOnly: boolean;
+};
